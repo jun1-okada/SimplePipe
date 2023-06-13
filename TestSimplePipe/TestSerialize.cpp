fa2a882fa25ed9165e837175977568d21f8fa52d
Jun-ichi Okada <okada@abt.jp>
2023-06-13 18:46:13 +0900

    書き込みキャンセル後に正しくデータ受信ができなくなる問題を修正。

---
+++ TestSimplePipe/TestSerialize.cpp
@@ -0,0 +1,234 @@
+﻿#include "pch.h"
+#include <windows.h>
+#include <string>
+#include <memory>
+#include <numeric>
+#include <algorithm>
+#include <memory.h>
+#include "CppUnitTest.h"
+#include "../inc/SimpleNamedPipe.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace abt::comm::simple_pipe::test::serialize
+{
+    using namespace abt::comm::simple_pipe;
+
+    std::wstring StrFromBuffer(const SimpleNamedPipeBase::Buffer& buffer)
+    {
+        return std::wstring(reinterpret_cast<LPCWSTR>(buffer.Pointer()), 0, buffer.Size() / sizeof(WCHAR));
+    }
+
+    TEST_CLASS(TestSerializer)
+    {
+        TEST_METHOD(Serialize)
+        {
+            TCHAR testData[] {L"ABCDEFGHIJKLMNOPQRSTUBWXYZ"};
+            constexpr DWORD splitSize = 10 * sizeof(WCHAR);
+            SimpleNamedPipeBase::Serializer serializer(SimpleNamedPipeBase::Buffer(testData,sizeof(testData) - sizeof(WCHAR)), splitSize);
+            {
+                auto [buffer, header] = serializer.Next();
+                Assert::IsFalse(buffer.Empty());
+                Assert::AreEqual(std::wstring(L"ABCDEFGHIJ"), StrFromBuffer(buffer));
+                Assert::IsTrue(header.info.startBit);
+                Assert::IsFalse(header.info.endBit);
+                Assert::IsFalse(header.info.cancelBit);
+                Assert::AreEqual(static_cast<size_t>(header.size), SimpleNamedPipeBase::HeaderSize + splitSize);
+                Assert::AreEqual(static_cast<size_t>(header.DataOffset()), SimpleNamedPipeBase::HeaderSize);
+                Assert::AreEqual(static_cast<size_t>(splitSize), header.DataSize());
+            }
+            {
+                auto [buffer, header] = serializer.Next();
+                Assert::IsFalse(buffer.Empty());
+                Assert::AreEqual(std::wstring(L"KLMNOPQRST"), StrFromBuffer(buffer));
+                Assert::IsFalse(header.info.startBit);
+                Assert::IsFalse(header.info.endBit);
+                Assert::IsFalse(header.info.cancelBit);
+                Assert::AreEqual(static_cast<size_t>(header.size), SimpleNamedPipeBase::HeaderSize + splitSize);
+                Assert::AreEqual(static_cast<size_t>(header.DataOffset()), SimpleNamedPipeBase::HeaderSize);
+                Assert::AreEqual(static_cast<size_t>(splitSize), header.DataSize());
+            }
+            {
+                auto [buffer, header] = serializer.Next();
+                Assert::IsFalse(buffer.Empty());
+                Assert::AreEqual(std::wstring(L"UBWXYZ"), StrFromBuffer(buffer));
+                Assert::IsFalse(header.info.startBit);
+                Assert::IsTrue(header.info.endBit);
+                Assert::IsFalse(header.info.cancelBit);
+                Assert::AreEqual(static_cast<size_t>(header.size), SimpleNamedPipeBase::HeaderSize + 6 * sizeof(WCHAR));
+                Assert::AreEqual(static_cast<size_t>(header.DataOffset()), SimpleNamedPipeBase::HeaderSize);
+                Assert::AreEqual(static_cast<size_t>(6 * sizeof(WCHAR)), header.DataSize());
+            }
+            {
+                auto [buffer, header] = serializer.Next();
+                Assert::IsTrue(buffer.Empty());
+            }
+            {
+                auto [buffer, header] = serializer.Next();
+                Assert::IsTrue(buffer.Empty());
+            }
+        }
+        TEST_METHOD(SerializeSingle)
+        {
+            TCHAR testData[]{ L"ABCDEFGHIJKLMNOPQRSTUBWXYZ" };
+            constexpr DWORD splitSize = 26 * sizeof(WCHAR);
+            SimpleNamedPipeBase::Serializer serializer(SimpleNamedPipeBase::Buffer(testData, sizeof(testData) - sizeof(WCHAR)), splitSize);
+            {
+                auto [buffer, header] = serializer.Next();
+                Assert::IsFalse(buffer.Empty());
+                Assert::AreEqual(std::wstring(L"ABCDEFGHIJKLMNOPQRSTUBWXYZ"), StrFromBuffer(buffer));
+                Assert::IsTrue(header.info.startBit);
+                Assert::IsTrue(header.info.endBit);
+                Assert::IsFalse(header.info.cancelBit);
+                Assert::AreEqual(static_cast<size_t>(header.size), SimpleNamedPipeBase::HeaderSize + splitSize);
+                Assert::AreEqual(static_cast<size_t>(header.DataOffset()), SimpleNamedPipeBase::HeaderSize);
+                Assert::AreEqual(static_cast<size_t>(splitSize), header.DataSize());
+            }
+            {
+                auto [buffer, header] = serializer.Next();
+                Assert::IsTrue(buffer.Empty());
+            }
+            {
+                auto [buffer, header] = serializer.Next();
+                Assert::IsTrue(buffer.Empty());
+            }
+        }
+        TEST_METHOD(SerializeEmpty)
+        {
+            SimpleNamedPipeBase::Serializer serializer(SimpleNamedPipeBase::Buffer(nullptr, 0), 10);
+            {
+                auto [buffer, header] = serializer.Next();
+                Assert::IsTrue(buffer.Empty());
+            }
+            {
+                auto [buffer, header] = serializer.Next();
+                Assert::IsTrue(buffer.Empty());
+            }
+        }
+    };
+
+    class PacketBuidler
+    {
+    private:
+        SimpleNamedPipeBase::Buffer buffer;
+        const DWORD splitSize;
+        bool beginning{ true };
+        std::vector<BYTE> work;
+    public:
+        PacketBuidler(SimpleNamedPipeBase::Buffer buffer, DWORD splitSize)
+            : buffer(buffer)
+            , splitSize(splitSize)
+        {}
+        const SimpleNamedPipeBase::Packet* Next()
+        {
+            if (buffer.Empty()) {
+                return nullptr;
+            }
+            work.clear();
+            auto size = (std::min)(static_cast<size_t>(splitSize), buffer.Size());
+            auto comsumed = buffer.Consume(size);
+            auto header = SimpleNamedPipeBase::Header::Create(static_cast<DWORD>(size), beginning, buffer.Empty());
+            auto p = reinterpret_cast<const BYTE*>(&header);
+            work.insert(work.end(), p, p + sizeof(header));
+            work.insert(work.end(), comsumed.Begin(), comsumed.End());
+            beginning = false;
+            return reinterpret_cast<const SimpleNamedPipeBase::Packet*>(&work[0]);
+        }
+    };
+
+    TEST_CLASS(TestDeerializer)
+    {
+        TEST_METHOD(Deserialize)
+        {
+            TCHAR testData1[]{ L"ABCDEFGHIJKLMNOPQRSTUVWXYZ" };
+            SimpleNamedPipeBase::Buffer testBuffer1(testData1, sizeof(testData1) - sizeof(WCHAR));
+            TCHAR testData2[]{ L"abcdefghijklmnopqrstuvwxyz" };
+            SimpleNamedPipeBase::Buffer testBuffer2(testData2, sizeof(testData2) - sizeof(WCHAR));
+
+            int progress = 1;
+
+            SimpleNamedPipeBase::Deserializer deserializer(1024, 1024, [&](auto buf) {
+                if (progress == 1) {
+                    Assert::AreEqual(std::wstring(testData1), StrFromBuffer(buf));
+                    progress = 2;
+                }
+                else if (progress == 2) {
+                    Assert::AreEqual(std::wstring(testData2), StrFromBuffer(buf));
+                    progress = 3;
+                }
+            });;
+            PacketBuidler builder1(testBuffer1, 10 * sizeof(WCHAR));
+            Assert::IsTrue(deserializer.Feed(builder1.Next()));
+            Assert::IsTrue(deserializer.Feed(builder1.Next()));
+            Assert::IsTrue(deserializer.Feed(builder1.Next()));
+
+            PacketBuidler builder2(testBuffer2, 10 * sizeof(WCHAR));
+            Assert::IsTrue(deserializer.Feed(builder2.Next()));
+            Assert::IsTrue(deserializer.Feed(builder2.Next()));
+            Assert::IsTrue(deserializer.Feed(builder2.Next()));
+
+            Assert::AreEqual(3, progress);
+        }
+
+        TEST_METHOD(DeserializeSingle)
+        {
+            TCHAR testData1[]{ L"ABCDEFGHIJKLMNOPQRSTUVWXYZ" };
+            SimpleNamedPipeBase::Buffer testBuffer1(testData1, sizeof(testData1) - sizeof(WCHAR));
+            TCHAR testData2[]{ L"abcdefghijklmnopqrstuvwxyz" };
+            SimpleNamedPipeBase::Buffer testBuffer2(testData2, sizeof(testData2) - sizeof(WCHAR));
+
+            int progress = 1;
+
+            SimpleNamedPipeBase::Deserializer deserializer(1024, 1024, [&](auto buf) {
+                if (progress == 1) {
+                    Assert::AreEqual(std::wstring(testData1), StrFromBuffer(buf));
+                    progress = 2;
+                }
+                else if (progress == 2) {
+                    Assert::AreEqual(std::wstring(testData2), StrFromBuffer(buf));
+                    progress = 3;
+                }
+            });;
+            PacketBuidler builder1(testBuffer1, static_cast<DWORD>(testBuffer1.Size()));
+            Assert::IsTrue(deserializer.Feed(builder1.Next()));
+
+            PacketBuidler builder2(testBuffer2, static_cast<DWORD>(testBuffer2.Size()));
+            Assert::IsTrue(deserializer.Feed(builder2.Next()));
+
+            Assert::AreEqual(3, progress);
+        }
+
+        TEST_METHOD(DeserializeCancel)
+        {
+            TCHAR testData1[]{ L"ABCDEFGHIJKLMNOPQRSTUVWXYZ" };
+            SimpleNamedPipeBase::Buffer testBuffer1(testData1, sizeof(testData1) - sizeof(WCHAR));
+            TCHAR testData2[]{ L"abcdefghijklmnopqrstuvwxyz" };
+            SimpleNamedPipeBase::Buffer testBuffer2(testData2, sizeof(testData2) - sizeof(WCHAR));
+
+            int progress = 1;
+
+            SimpleNamedPipeBase::Deserializer deserializer(1024, 1024, [&](auto buf) {
+                Assert::AreNotEqual(1, progress);
+                if (progress == 2) {
+                    Assert::AreEqual(std::wstring(testData2), StrFromBuffer(buf));
+                    progress = 3;
+                }
+            });;
+
+            auto cancelHeader = SimpleNamedPipeBase::Header::CreateCancel();
+            
+            PacketBuidler builder1(testBuffer1, 10 * sizeof(WCHAR));
+            Assert::IsTrue(deserializer.Feed(builder1.Next()));
+            Assert::IsFalse(deserializer.Feed(reinterpret_cast<const SimpleNamedPipeBase::Packet*>(&cancelHeader)));
+
+            progress = 2;
+
+            PacketBuidler builder2(testBuffer2, 10 * sizeof(WCHAR));
+            Assert::IsTrue(deserializer.Feed(builder2.Next()));
+            Assert::IsTrue(deserializer.Feed(builder2.Next()));
+            Assert::IsTrue(deserializer.Feed(builder2.Next()));
+
+            Assert::AreEqual(3, progress);
+        }
+    };
+}
--- TestSimplePipe/TestSimplePipe.cpp
+++ TestSimplePipe/TestSimplePipe.cpp
@@ -12,6 +12,8 @@
 #include <ppl.h>
 #include <ppltasks.h>
 #include "CppUnitTest.h"
+
+#define SNP_TEST_MODE   //テストモード有効
 #include "../inc/SimpleNamedPipe.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
@@ -470,6 +472,92 @@ namespace abt::comm::simple_pipe::test
         TEST_METHOD(WriteCancel)
         {
             auto pipeName = std::wstring(L"\\\\.\\pipe\\") + winrt::to_hstring(winrt::Windows::Foundation::GuidHelper::CreateNewGuid());
+            concurrency::task<void> serverErrTask = concurrency::task_from_result();
+            concurrency::event closedEvent;
+            constexpr size_t BUFFER_SIZE = 512;
+
+            std::vector<int> expected(512*4);
+            for (int i = 0; i < expected.size(); ++i) {
+                expected[i] = std::rand();
+            }
+            std::vector<int> actual;
+            actual.reserve(expected.size());
+
+            SimpleNamedPipeServer<BUFFER_SIZE> server(pipeName.c_str(), nullptr, [&](auto& ps, const auto& param) {
+                switch (param.type) {
+                case PipeEventType::CONNECTED:
+                    break;
+                case PipeEventType::DISCONNECTED:
+                    closedEvent.set();
+                    break;
+                case PipeEventType::RECEIVED:
+                    //エコーバック
+                    ps.WriteAsync(param.readBuffer, param.readedSize).wait();
+                    break;
+                case PipeEventType::EXCEPTION:
+                    //監視タスクで例外発生
+                    if (param.errTask) {
+                        serverErrTask = param.errTask.value();
+                    }
+                    break;
+                }
+            });
+            concurrency::task<void> clientErrTask = concurrency::task_from_result();
+            concurrency::event echoComplete;
+
+            SimpleNamedPipeClient<BUFFER_SIZE> client(pipeName.c_str(), [&](auto& ps, const auto& param) {
+                switch (param.type) {
+                case PipeEventType::DISCONNECTED:
+                    break;
+                case PipeEventType::RECEIVED:
+                {
+                    auto p = reinterpret_cast<const int*>(param.readBuffer);
+                    actual.insert(actual.end(), p, p + (param.readedSize / sizeof(int)));
+                    echoComplete.set();
+                }
+                break;
+                case PipeEventType::EXCEPTION:
+                    //監視タスクで例外発生
+                    if (param.errTask) {
+                        clientErrTask = param.errTask.value();
+                    }
+                    break;
+                }
+            });
+
+            concurrency::cancellation_token_source cts;
+            client.onWritePacket = [&]() {
+                //送信途中でキャンセルする
+                cts.cancel();
+            };
+
+            Assert::ExpectException<concurrency::task_canceled>([&]() {
+                client.WriteAsync(&expected[0], expected.size() * sizeof(int), cts.get_token()).get();
+            });
+
+            //0.1秒待機して受信データが来なければキャンセル成功
+            Assert::AreEqual(concurrency::COOPERATIVE_WAIT_TIMEOUT, echoComplete.wait(100));
+
+            //キャンセル後に送信可能かチェック
+            client.onWritePacket = nullptr;
+
+            client.WriteAsync(&expected[0], expected.size() * sizeof(int)).wait();
+            Assert::AreNotEqual(concurrency::COOPERATIVE_WAIT_TIMEOUT, echoComplete.wait(1000));
+            Assert::IsTrue(expected == actual);
+
+            client.Close();
+
+            Assert::AreNotEqual(concurrency::COOPERATIVE_WAIT_TIMEOUT, closedEvent.wait(1000));
+            server.Close();
+
+            serverErrTask.wait();
+            clientErrTask.wait();
+
+        }
+
+        TEST_METHOD(WriteCancelImmediate)
+        {
+            auto pipeName = std::wstring(L"\\\\.\\pipe\\") + winrt::to_hstring(winrt::Windows::Foundation::GuidHelper::CreateNewGuid());
 
             concurrency::task<void> serverErrTask = concurrency::task_from_result();
             concurrency::event closedEvent;
@@ -497,7 +585,7 @@ namespace abt::comm::simple_pipe::test
  
             concurrency::task<void> clientErrTask = concurrency::task_from_result();
             concurrency::event echoComplete;
-            std::wstring echoMessage;
+            std::vector<int> actual;
 
             SimpleNamedPipeClient<BUFFER_SIZE> client(pipeName.c_str(), [&](auto& ps, const auto& param) {
                 switch (param.type) {
@@ -505,8 +593,8 @@ namespace abt::comm::simple_pipe::test
                     break;
                 case PipeEventType::RECEIVED:
                 {
-                    std::wstring m(reinterpret_cast<LPCWSTR>(param.readBuffer), 0, param.readedSize / sizeof(WCHAR));
-                    echoMessage = m;
+                    auto p = reinterpret_cast<const int*>(param.readBuffer);
+                    actual.insert(actual.end(), p, p + (param.readedSize / sizeof(int)));
                     echoComplete.set();
                 }
                 break;
@@ -535,9 +623,14 @@ namespace abt::comm::simple_pipe::test
             }
 
             //1秒待機して受信データが来なければキャンセル成功
-            if (concurrency::COOPERATIVE_WAIT_TIMEOUT != echoComplete.wait(1000)) {
-                Assert::Fail();
-            }
+            Assert::AreEqual(concurrency::COOPERATIVE_WAIT_TIMEOUT, echoComplete.wait(1000));
+
+            //キャンセル後に通信可能かチェック
+            client.WriteAsync(&expected[0], SAMPLE_BYTE_SIZE).wait();
+
+            Assert::AreNotEqual(concurrency::COOPERATIVE_WAIT_TIMEOUT, echoComplete.wait(1000));
+            Assert::IsTrue(std::equal(expected.get(), expected.get() + SAMPLE_SIZE, actual.begin(), actual.end()));
+
             client.Close();
 
             if (concurrency::COOPERATIVE_WAIT_TIMEOUT == closedEvent.wait(1000)) {
